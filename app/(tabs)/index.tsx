4da287e5c9acf4e714aa5c929baef350c7c0b6ab
阿菜 Cai <jimmyrss1102@gmail.com>
2024-12-06 02:46:06 +0800

    feat: 录音和停止录音

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,9 @@
 import 'react-native-gesture-handler';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import Animated, {
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
@@ -17,35 +18,101 @@ import { Audio } from "expo-av";
 
 export default function App() {
   const pressed = useSharedValue<boolean>(false);
-  const [isAllowRecord, setAllowRecord] = useState("undetermined");
+
+  const [recording, setRecording] = useState<Audio.Recording | null>(null);
+  const [isAllowRecord, setAllowRecord] = useState<Audio.PermissionStatus | 'undetermined'>("undetermined");
+  const [recordingStatus, setRecordingStatus] = useState<Audio.RecordingStatus | null>(null);
 
   useEffect(() => {
     const requestPermissions = async () => {
       try {
         const response = await Audio.requestPermissionsAsync();
-        // console.log("Permissions response", response);
-        // setAllowRecord(response.status);
+        console.log("Permissions response", response);
+        setAllowRecord(response.status);
       } catch (error) {
         // setAllowRecord('error');
-        // console.error("Failed to get permissions", error);
+        console.error("Failed to get permissions", error);
       }
     };
 
     requestPermissions();
   }, []);
 
+  /** 开始录音 */
+  const startRecording = async () => {
+    try {
+      if (recording && recordingStatus?.canRecord) {
+        await recording.startAsync();
+        return;
+      }
+      await Audio.setAudioModeAsync({
+        playsInSilentModeIOS: true,
+        allowsRecordingIOS: true,
+      });
+
+      // 创建并准备录音 
+      const newRecording = new Audio.Recording();
+      setRecording(newRecording);
+
+      await newRecording.prepareToRecordAsync(Audio.RecordingOptionsPresets.HIGH_QUALITY);
+      newRecording.setOnRecordingStatusUpdate(setRecordingStatus);// 设置录音状态更新的回调函数
+      await newRecording.startAsync();
+    } catch (error) {
+      console.error("Failed to start recording", error);
+      // todo: 错误通知
+    }
+  };
+
+  /** 停止录音 */
+  const stopRecording = async () => {
+    if (!recording) return;
+
+    try {
+      await recording.stopAndUnloadAsync();
+      console.log(`Recorded URI: ${recording.getURI()}`);
+    } catch (error) {
+      console.error("Failed to stop recording", error);
+      // todo: 错误通知
+    }
+  };
+
+  /** 播放录音 */
+  const playSound = async () => {
+    if (!recording || !recordingStatus?.isDoneRecording) return
+
+    try {
+      const { sound } = await recording.createNewLoadedSoundAsync();
+      await sound.playAsync();
+    } catch (error) {
+      console.error("Failed to play sound", error);
+      // todo: 错误通知
+    }
+  }
+
   const tap = Gesture.LongPress()
     .onBegin(() => {
       pressed.value = true;
+      startRecording();
     })
     .onFinalize(() => {
       pressed.value = false;
+      stopRecording();
     });
 
-  const animatedStyles = useAnimatedStyle(() => ({
-    backgroundColor: pressed.value ? '#CC3363' : '#B58DF1',
-    transform: [{ scale: withTiming(pressed.value ? 1.2 : 1) }],
-  }));
+  const circleColor = useDerivedValue(() => {
+    if (isAllowRecord === "granted") {
+      return pressed.value ? '#CC3363' : '#B58DF1';
+    }
+    return '#A9A9A9';
+  });
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      backgroundColor: circleColor.value,
+      transform: [{ scale: withTiming(pressed.value ? 1.2 : 1) }],
+    };
+  });
+
 
   return (
     <GestureHandlerRootView style={styles.container}>
